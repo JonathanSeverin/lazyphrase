697559dc68fd48091b33683036563d4dbb9f57e3
Jonathan Severin <jonathansev19@gmail.com>
2026-06-26 12:24:03 +0200

    feat: add basic keyboard listener functions
    Refs #1

---
+++ core/hotkeys.py
@@ -0,0 +1,27 @@
+from pynput import keyboard
+
+
+def on_press(key):
+  try:
+    print(f'alphanumeric key {key.char} pressed')
+  except AttributeError:
+    print(f'special key {key} pressed')
+
+
+
+def on_release(key):
+  print(f'{key} released')
+  if key == keyboard.Key.esc:
+    # Stop listener
+    return False
+    ## Just for testing. Should not stop the listener in production code.
+
+
+
+listener = keyboard.Listener(
+  on_press=on_press,
+  on_release=on_release
+)
+
+listener.start()
+listener.join()
