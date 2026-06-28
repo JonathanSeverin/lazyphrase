2aadcc8ef7f75df3f94b74a357d675bc3e76806d
Jonathan Severin <jonathansev19@gmail.com>
2026-06-28 10:01:28 +0200

    fix: missing normalization of keys before adding to set
    Added a normalization function that keys has to go through before
    beeing added or deleted from the set, to ensure that pressed keys
    also are beeing removed from the set.
    
    Refs #1

--- core/hotkeys.py
+++ core/hotkeys.py
@@ -2,19 +2,17 @@ from pynput import keyboard
 
 
 def build_hotkey_string(current_keys):
-  elements = []
-
-  for key in current_keys:
-    try:
-      char = key.char
-      if char:
-        elements.append(str(char.lower()))
-    except AttributeError:
-      lst = str(key).split(".")
-      if len(lst) == 2 and lst[0] == "Key": elements.append(lst[1])
-
+  elements = list(current_keys)
   elements.sort() # ensures consistent order for the hotkey string (since sets are unordered and could lead to different strings for the same hotkey combination)
   return "+".join(elements)
+
+
+def normalize_key(key):
+  try:
+    if key.char: return str(key.char).lower()
+  except AttributeError:
+    pass
+  return str(key).replace("Key.", "")
 
 
 def start_listener(on_hotkey):
@@ -22,14 +20,14 @@ def start_listener(on_hotkey):
   current_keys = set()
 
   def on_press(key):
-    current_keys.add(key)
+    current_keys.add(normalize_key(key))
     if len(current_keys) > 1:
       hotkey_string = build_hotkey_string(current_keys) 
       on_hotkey(hotkey_string)
     
 
   def on_release(key):
-    current_keys.discard(key)
+    current_keys.discard(normalize_key(key))
     if key == keyboard.Key.esc:
       # Stop listener
       return False
