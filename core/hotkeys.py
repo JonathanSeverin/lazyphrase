4254297f351f3718ee7fb1f10c18136c0b24a3dc
Jonathan Severin <jonathansev19@gmail.com>
2026-06-28 13:34:16 +0200

    feat: add hardcoded data to hotkey dict + minor fixes
    Refs #1

--- core/hotkeys.py
+++ core/hotkeys.py
@@ -8,12 +8,16 @@ def build_hotkey_string(current_keys):
 
 
 def normalize_key(key):
+  temp = None
+
   try:
     if key.char: return str(key.char).lower()
   except AttributeError:
-    lst = str(key).split(".")
-    temp = ""
-    if len(lst) > 1 and lst[0] == "Key": temp = lst[1]
+    pass
+
+  lst = str(key).split(".")
+  if len(lst) > 1 and lst[0] == "Key": 
+    temp = lst[1]
 
     if temp.endswith("_l") or temp.endswith("_r"):
       temp = temp[:-2]  # Remove the "_l" or "_r" ending
@@ -27,28 +31,30 @@ def start_listener(on_hotkey):
   last_hotkey = None
 
   def on_press(key):
-    nonlocal last_hotkey
+      nonlocal last_hotkey
 
-    current_keys.add(normalize_key(key))
-    if len(current_keys) > 1:
-      hotkey_string = build_hotkey_string(current_keys) 
+      current_keys.add(normalize_key(key))
 
-      if hotkey_string != last_hotkey:
-        on_hotkey(hotkey_string)
-        last_hotkey = hotkey_string
+      if len(current_keys) > 1:
+        hotkey_string = build_hotkey_string(current_keys) 
 
+        if hotkey_string != last_hotkey:
+          on_hotkey(hotkey_string)
+          last_hotkey = hotkey_string
+          current_keys.clear()  # Clear the set after triggering the hotkey. See note 1 at end of file for explanation
+  
 
   def on_release(key):
     nonlocal last_hotkey
 
     current_keys.discard(normalize_key(key))
-    last_hotkey = None
+    if not current_keys: last_hotkey = None
 
     if key == keyboard.Key.esc:
       # Stop listener
       return False
       ## Just for testing. Should not stop the listener in production code.
-
+  
 
   listener = keyboard.Listener(
     on_press=on_press,
@@ -57,3 +63,15 @@ def start_listener(on_hotkey):
   listener.start()
   listener.join()
 
+
+"""  
+Note1: Some key combinations (e.g. Ctrl+S) may behave differently when the program
+is run inside a terminal. In many Unix-like systems, Ctrl+S is used for flow control
+(XOFF), which pauses terminal input/output. 
+Press Ctrl+Q to resume.
+
+Additionally, current_keys is cleared after a hotkey is triggered to ensure a clean
+state and avoid issues if key release events are missed by the system.
+Extra layer in additon to on_release to ensure that current_keys is cleared after a hotkey is triggered.
+This is to avoid issues if key release events are missed by the system.
+"""
