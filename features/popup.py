bf76ad1fe490b64aa22f56c85e8b19ad93399355
Jonathan Severin <jonathansev19@gmail.com>
2026-06-29 21:30:18 +0200

    feat: implement action handling after popup element click
    Click on phrase in popup now actions to insert_text() function.
    The phrase gets displayed in terminal, but not in other text input sections!
    This must be fixed.
    
    Refs #7

--- features/popup.py
+++ features/popup.py
@@ -1,12 +1,50 @@
 import tkinter as tk
 
+from core.expander import insert_text
+
 def show_popup(phrases):
   root = tk.Tk()
-  root.geometry("400x300")
+  root.minsize("150", "200")
+  root.geometry("250x300")
+   
   listbox = tk.Listbox(root)
 
   for c, phrase in enumerate(phrases):
     listbox.insert(c, phrase.title)
 
-  listbox.pack()
-  root.mainloop() # needed? What if not added? If added, how to kill?
+  
+  def on_click(event):
+    selection = listbox.curselection()
+    if selection:
+      index = selection[0]
+      phrase = phrases[index]
+      insert_text(phrase.content)
+      root.destroy()  # Close the popup after selection
+
+
+  def on_hover(event):
+    index = listbox.nearest(event.y)
+    listbox.selection_clear(0, tk.END)
+    listbox.selection_set(index)
+  
+
+  # Clicks outside of popup should close it. Need to implement that
+  def on_close():
+    root.destroy()  # Close the popup when the window is closed
+
+
+
+
+  listbox.bind("<Button-1>", on_click)
+  listbox.bind("<Motion>", on_hover)
+
+
+  listbox.pack(fill=tk.BOTH, expand=True)
+  root.mainloop()
+
+
+""" 
+skjønne koden
+fikse display av koden, ser helt ass ut
+fikse hvorfor den ikke limer inn andre steder enn i terminal nå
+ """
