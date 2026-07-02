0c86301b015db2a5556c51488365b94ac4ea436a
Jonathan Severin <jonathansev19@gmail.com>
2026-07-02 21:30:34 +0200

    feat: Add pathlib for portable phrase storage path
    This ensures potentially missing folders to the standardized
    placement for the json file is created. Also some issues in both
    loading and saving fixed. Saving and loading works fine for the MVP.
    
    Refs #9

--- storage/file_store.py
+++ storage/file_store.py
@@ -1,33 +1,43 @@
 import json
+from pathlib import Path
 
-from models import Phrase
+from models.phrase import Phrase
 
-GLOBAL_PATH_TO_PHRASES = "~/.local/share/phraseexpander/phrases.json"
+path = (
+  Path.home() 
+  / ".local" 
+  / "share" 
+  / "phraseexpander" 
+  / "phrases.json"
+)
+
+path.parent.mkdir(parents=True, exist_ok=True)  # Ensure the directory exists
 
 
 def save_to_file(data):
-  try:
-    with open(GLOBAL_PATH_TO_PHRASES, "w") as f:
-      destruct = []
-      for key, value in data.items():
-        phr_to_dict = {}
-        phr_to_dict.update(key, value)
-        destruct.append(phr_to_dict)
-
-      json.dump(destruct, f)
-  except (FileNotFoundError) as e:
-    print(f"failed to save phrases {e}")
-
+  with open(path, "w") as f:
+    destruct = []
+    for key, value in data.items():
+      for phrase in value:
+        destruct.append({
+          "title": phrase.title,
+          "content": phrase.content,
+          "hotkey": phrase.hotkey,
+          "is_active": phrase.is_active
+        })
+    json.dump(destruct, f)
+  
 
 def load_from_file():
   try:
-    with open(GLOBAL_PATH_TO_PHRASES, "r") as f:
+    with open(path, "r") as f:
       loaded = json.load(f)
       dict_to_phrase = [Phrase(**phrase) for phrase in loaded]
       out_dict = {}
       for p in dict_to_phrase:
         if p.hotkey in out_dict: out_dict[p.hotkey].append(p)
-        else: out_dict.update(p.hotkey, [p])
+        else: out_dict[p.hotkey] = [p]
       return out_dict
-  except (FileNotFoundError) as e: 
+  except (FileNotFoundError, json.JSONDecodeError) as e: 
     print(f"Could not load phrases from file {e}")
+    return {}
