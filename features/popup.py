e2cfa8a498899dc23d3fe59c9cf4187de060e9d4
Jonathan Severin <jonathansev19@gmail.com>
2026-07-04 11:08:16 +0200

    feat: make arrow keys switch between selected phrases
    Now both moving the mouse over a phrase or pressing arrow key up/down
    will change selection of phrase
    
    Refs #11

--- features/popup.py
+++ features/popup.py
@@ -25,18 +25,28 @@ def show_popup(phrases):
       insert_text(phrase.content)
       
 
-
   def on_hover(event):
     index = listbox.nearest(event.y)
     listbox.selection_clear(0, tk.END)
     listbox.selection_set(index)
   
 
+  def on_arrow_key_pressed(event):
+    selection = listbox.curselection()
+    if selection:
+      index = selection[0]
+      if event.keysym == "Up" and index > 0:
+        listbox.selection_clear(0, tk.END)
+        listbox.selection_set(index - 1)
+      elif (event.keysym == "Down") and ((index + 1) < listbox.size()):
+        listbox.selection_clear(0, tk.END)
+        listbox.selection_set(index + 1)
+
+
+
+
   def on_close():
     root.destroy()  # Close the popup when the window is closed
-
-  # Clicks outside of popup should close it. Need to implement that
-  #def on_click_outside(event):
 
   def check_focus():
     if root.focus_displayof() is None:
@@ -46,10 +56,13 @@ def show_popup(phrases):
   listbox.bind("<Button-1>", on_click)
   listbox.bind("<Return>", on_click)  # Bind Enter key to selection
   listbox.bind("<Motion>", on_hover)
+  listbox.bind("Up", on_arrow_key_pressed)
+  listbox.bind("Down", on_arrow_key_pressed)
   root.bind("<FocusOut>", lambda event: root.after(100, check_focus))  # Close the popup when it loses focus
 
+
+
   root.attributes("-topmost", True)  # Keep the popup on top of other windows
-  #root.focus_force()  # Force focus on the popup window
   root.protocol("WM_DELETE_WINDOW", on_close)  # Handle window close event
 
   root.mainloop()
