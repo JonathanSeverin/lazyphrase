09acb2fca24c8ba114b1c1698637d959cf53ce2a
Jonathan Severin <jonathansev19@gmail.com>
2026-08-18 20:03:42 +0200

    feat: make sure first phrase in popup is selected
    This allows a user to hit a hotkey --> popup opens --> hit enter to paste
    the first phrase into active input field.
    
    Refs #12

--- features/popup.py
+++ features/popup.py
@@ -25,6 +25,7 @@ def create_popup(root, phrases, on_popup_close, on_popup_geometry_update=None, i
   for c, phrase in enumerate(phrases):
     listbox.insert(c, phrase.title)
 
+  listbox.selection_set(0)  # Select the first item by default
   listbox.focus_set()  # Set focus to the listbox so it can receive keyboard events
 
   
